d00559cacd17f3729e15f480f34a0fb622a82d21
Krystsina Gerko <krystsina-belfresh@mail.ru>
2019-03-24 23:55:03 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let open = bracketsConfig.map((elem) => elem[0]);
+  let close = bracketsConfig.map((elem) => elem[1]);
+  let arr = str.split('');
+
+  for (let i = 0; i < arr.length; i++) {
+      if(close.includes(arr[i])) { 
+          let n = i - 1;
+          while(arr[n] == 0) n--;
+          if(arr[n] == open[close.indexOf(arr[i])]) {
+            arr[n] = 0;
+            arr[i] = 0;
+          }
+      }
+  }
+  return arr.every((elem) => elem == 0);
 }
